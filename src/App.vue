8128c2b9c1d0476a34e27c3353610967212b29f1
こじまはやと <156152055+SuperKojima@users.noreply.github.com>
2024-11-01 06:29:15 +0000

    tailiwind

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,14 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125">
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">
-          Home
-        </RouterLink>
-        <RouterLink to="/about">
-          About
-        </RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <h1 class="text-lg font-bold">
+      Vue Codespaces
+    </h1>
+    <p>
+      HelloWorld
+    </p>
+  </div>
 </template>
 
 <style scoped>
